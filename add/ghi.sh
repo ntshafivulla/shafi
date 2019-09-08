15565dec18b13210cd76a37ad10575693be85491
ntshafivulla <54802409+ntshafivulla@users.noreply.github.com>
2019-09-08 21:39:17 +0530

    Update ghi.sh

--- add/ghi.sh
+++ add/ghi.sh
@@ -1,13 +1,10 @@
 echo "hai nt shafivulla"
-echo "this is my project"
-echo "this new process"
-echo "please do this project"
-echo "this new compliment"
-echo "this is new administrator"
+echo "this is my proje
 echo "new attachment"
-echo "new entity"
+echo "new molecule
 echo "hello shafivulla"
 echo "create new repo"
 echo "new commit"
 echo "new present"
+echo "next replacement"
 
